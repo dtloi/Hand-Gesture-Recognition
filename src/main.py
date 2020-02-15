f0397ae9f591c5f0eb74698ac808b1829f4b5caf
Sehej Sohal <seahedge2@me.com>
2020-02-15 11:15:53 -0800

    added a model to train using averages

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
 import numpy as np
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Activation, Flatten
+import matplotlib.pyplot as plt
+import warnings
+
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Input, Dense, Dropout, Activation, Flatten
 
 from readData import readPinchFromNP
 
+warnings.filterwarnings('ignore') #we'll probably need to turn this back on
 def main():
 
     preprocessedData = readPinchFromNP()
@@ -11,11 +15,54 @@ def main():
     labels = preprocessedData[0] # ((n, 5)) numpy array  of labels
     data = preprocessedData[1] # ((n, 8)) numpy array of averages of every session
 
+    train_labels, test_labels = np.split(labels, [2000, ])
+    train_data, test_data = np.split(data, [2000, ])
 
-    print(labels.shape)
-    print(data.shape)
+    print(train_labels.shape)
+    print(test_labels.shape)
+    print(train_data.shape)
+    print(test_data.shape)
 
-    #now we can start fucking with keras
+    input = Input(shape=(8,))
+    x1 = Dense(128, activation='relu')(input)
+    x2 = Dense(64, activation='relu')(input)
+    x3 = Dense(32, activation='relu')(input)
+    x4 = Dense(16, activation='relu')(input)
+    x5 = Dense(8, activation='relu')(input)
+    x6 = Dense(5, activation='sigmoid')(input)
+
+    model = Model(inputs=input, outputs=x6)
+
+    model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['acc'])
+
+    history = model.fit(train_data, train_labels, validation_split=0.1, epochs=150, batch_size=32)
+
+    train_loss, train_acc = model.evaluate(train_data, train_labels)
+    test_loss, test_acc = model.evaluate(test_data, test_labels)
+
+    print('Training set accuracy:', train_acc)
+    print('Test set accuracy:', test_acc)
+
+    model.summary()
+
+    # The history of our accuracy during training.
+    print(history.history.keys())
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('Model Accuracy')
+    plt.ylabel('Accuracy')
+    plt.xlabel('Number of epochs')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+
+    # The history of our cross-entropy loss during training.
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('Model Loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Number of epochs')
+    plt.legend(['train', 'test'], loc='upper right')
+    plt.show()
 
 
 if __name__ == "__main__":
